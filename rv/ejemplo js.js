943fdcb82fa07c5a786a7681bcec46e0d46f8cd5
Rasdenmartinez <libre93@live.com.mx>
2016-08-16 15:14:27 -0500

    Update ejemplo js.js

--- rv/ejemplo js.js
+++ rv/ejemplo js.js
@@ -2,3 +2,19 @@ var miCanvas = document.getElementById("miCanvas");
 var contexto=miCanvas.getContext("2d");
 contexto.fillStyle="red";
 contexto.fillRect(30,30,50,50);
+
+
+<!doctype html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<title>Ejemplo con guion externo</title>
+</head>
+<body>
+<h1> Ejemplo con guión externo</h1>
+<canvas id="miCanvas" width="100" height="100">
+&iexcl; no funciona!
+</canvas>
+<script src="ejemplo-3.js></script>
+</body>
+</html>
