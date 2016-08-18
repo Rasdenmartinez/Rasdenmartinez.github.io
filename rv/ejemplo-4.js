7be844bf5b77c551bbc0e8bedde7023beb431a11
Rasdenmartinez <libre93@live.com.mx>
2016-08-18 15:44:25 -0500

    Update ejemplo-4.js

--- rv/ejemplo-4.js
+++ rv/ejemplo-4.js
@@ -7,7 +7,7 @@ document.body.appendChild(renderizador.domElement);
 var forma= new THREE.BoxGeometry(1,1,1);
 var material = new THREE.MeshNormalMaterial();
 var cubo = new THREE.Mesh(forma, material);
-cubo.rotateX(-Math.PI/4.0);
-cubo.rotateY(Math.PI/4.0);
+cubo.rotateX(-Math.PI/4);
+cubo.rotateY(Math.PI/4);
 escena.add(cubo);
 renderizador.render(escena,camara)
