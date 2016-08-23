874eb102675aaf4b84049ac415ac02b883c2fbd2
Rasdenmartinez <libre93@live.com.mx>
2016-08-23 15:48:34 -0500

    Update ejemplo-geometry.js

--- rv/ejemplo-geometry.js
+++ rv/ejemplo-geometry.js
@@ -21,16 +21,15 @@ var material = new THREE.MeshNormalMaterials();
 var malla  = new THREE.Mesh(forma, material);
 malla.rotateX(Math.PI/4);
 
-var escena= new THREE.SCENE();
+var escena= new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize(window.innerHeight*.95,
-window.innerHeight*.95);
-document.boy.appendChild(renderizador.domElement);
-renderizador.render(escena, camera);
+renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
 
 
