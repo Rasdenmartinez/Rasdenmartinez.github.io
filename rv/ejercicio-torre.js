043a04601cda7216648425ef564738eaf5f18a19
Rasdenmartinez <libre93@live.com.mx>
2016-08-30 15:46:58 -0500

    Update ejercicio-torre.js

--- rv/ejercicio-torre.js
+++ rv/ejercicio-torre.js
@@ -1,11 +1,11 @@
 var torre = new THREE.CylinderBufferGeometry(5, 5, 20, 32);
 var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-var cylinder = new THREE.Mesh( geometry, material );
+var cylinder = new THREE.Mesh( torre, material );
 
 torre.translate(0,1,0);
 var torres = new THREE.Mesh(torre);
 var edificio = new THREE.Geometry();
-edificio.merge(torre.geometry, torre.matrix);
+edificio.merge(torres.geometry, torres.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var edificio = new THREE.Mesh(edificio, material);
