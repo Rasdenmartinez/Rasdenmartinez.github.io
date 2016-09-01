0123f997d15b5f3a236f8187b14d26df712fb4e9
Rasdenmartinez <libre93@live.com.mx>
2016-09-01 15:11:44 -0500

    Create estrella.js

---
+++ rv/estrella.js
@@ -0,0 +1,28 @@
+var forma = new THREE.Geometry();
+forma.vertices.push(new THREE.Vector3(0, 2, 0));
+forma.vertices.push(new THREE.Vector3(0.5, 1, 0));
+forma.vertices.push(new THREE.Vector3(-0.5, 1, 0));
+
+
+forma.faces.push(new THREE.Face3(0, 1, 2));
+
+
+forma.computeBoundingSphere();  
+forma.computeFaceNormals();
+
+
+
+var material = new THREE.MeshNormalMaterial();
+var malla  = new THREE.Mesh(forma, material);
+malla.rotateX(Math.PI/4);
+
+var escena= new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
