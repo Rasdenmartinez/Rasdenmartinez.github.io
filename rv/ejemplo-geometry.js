bd8d98f62bc930d32ea03c8e49f720744f069659
Rasdenmartinez <libre93@live.com.mx>
2016-09-01 15:27:49 -0500

    Update ejemplo-geometry.js

--- rv/ejemplo-geometry.js
+++ rv/ejemplo-geometry.js
@@ -7,6 +7,10 @@ forma.vertices.push(new THREE.Vector3(0, 1, 0));
 
 forma.faces.push(new THREE.Face3(3, 2, 1));
 forma.faces.push(new THREE.Face3(3, 1, 0));
+forma.faces.push(new THREE.Face3(3, 0, 4));
+forma.faces.push(new THREE.Face3(0, 1, 4));
+forma.faces.push(new THREE.Face3(1, 2, 4));
+forma.faces.push(new THREE.Face3(2, 3, 4));
 
 forma.computeBoundingSphere();  
 forma.computeFaceNormals();
