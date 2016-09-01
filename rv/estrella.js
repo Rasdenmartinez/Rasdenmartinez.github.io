4c223ee495bce53ae151f78066f2fb9c749f8e5d
Rasdenmartinez <libre93@live.com.mx>
2016-09-01 15:45:44 -0500

    Update estrella.js

--- rv/estrella.js
+++ rv/estrella.js
@@ -1,11 +1,14 @@
 var forma = new THREE.Geometry();
-forma.vertices.push(new THREE.Vector3(0, 2, 0));
-forma.vertices.push(new THREE.Vector3(1, 1, 0));
-forma.vertices.push(new THREE.Vector3(-1, 1, 0));
+forma.vertices.push(new THREE.Vector3(0, 1, 0));
+forma.vertices.push(new THREE.Vector3(1, 0, 0));
+forma.vertices.push(new THREE.Vector3(-1, 0, 0));
 
 
+forma.faces.push(new THREE.Face3(2, 1, 0));
+forma.faces.push(new THREE.Face3(2, 0, 1));
 forma.faces.push(new THREE.Face3(0, 1, 2));
-forma.faces.push(new THREE.Face3(2, 0 , 1));
+forma.faces.push(new THREE.Face3(1, 2, 0));
+
 
 forma.computeBoundingSphere();  
 forma.computeFaceNormals();
