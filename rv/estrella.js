30f0ccdd4056129cccebb1216bd3b1d1fbc24456
Rasdenmartinez <libre93@live.com.mx>
2016-09-01 15:52:07 -0500

    Update estrella.js

--- rv/estrella.js
+++ rv/estrella.js
@@ -20,7 +20,7 @@ var escena= new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.y=5;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*0.95, window.innerHeight*0.95);
