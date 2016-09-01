2a5350de79d689c46f9f3a28b9974ec7444895dc
Rasdenmartinez <libre93@live.com.mx>
2016-09-01 15:19:19 -0500

    Update estrella.js

--- rv/estrella.js
+++ rv/estrella.js
@@ -14,15 +14,15 @@ forma.computeFaceNormals();
 
 var material = new THREE.MeshNormalMaterial();
 var malla  = new THREE.Mesh(forma, material);
-malla.rotateY(Math.PI/4);
+malla.rotateX(Math.PI/4);
 
 var escena= new THREE.Scene();
 escena.add(malla);
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=10;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize(window.innerHeight*2, window.innerHeight*2);
+renderizador.setSize(window.innerHeight*0.95, window.innerHeight*0.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena, camara);
