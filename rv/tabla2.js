aaab1d3bf6a8cf7ecc622422e0e484ef7dc3d124
Rasdenmartinez <libre93@live.com.mx>
2016-09-08 15:49:05 -0500

    Update tabla2.js

--- rv/tabla2.js
+++ rv/tabla2.js
@@ -21,7 +21,7 @@
 				escena.add( line );
 				
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 5;
+camara.position.x = 5;
 
 renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
