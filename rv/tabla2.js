b676ea127b39e4d0d39b0a44726006d9874e3597
Rasdenmartinez <libre93@live.com.mx>
2016-09-08 16:07:11 -0500

    Update tabla2.js

--- rv/tabla2.js
+++ rv/tabla2.js
@@ -18,7 +18,7 @@
 
 				var line = new THREE.LineSegments( geometry, material );
 				
-				line.rotateY(Math.PI/4);
+				line.rotateX(Math.PI/4);
 				
 				var escena = new THREE.Scene();
 				escena.add( line );
