5ab568e881f7365a865aeb86df89328570c0ef5b
Rasdenmartinez <libre93@live.com.mx>
2016-09-20 15:03:39 -0500

    Update tabla2.js

--- rv/tabla2.js
+++ rv/tabla2.js
@@ -1,32 +1,344 @@
-	// grid
+var cubo1= new THREE.BoxGeometry(1,1,1);
+var cubo2= new THREE.BoxGeometry(1,1,1);
+var cubo3= new THREE.BoxGeometry(1,1,1);
+var cubo4= new THREE.BoxGeometry(1,1,1);
+var cubo5= new THREE.BoxGeometry(1,1,1);
+var cubo6= new THREE.BoxGeometry(1,1,1);
+var cubo7= new THREE.BoxGeometry(1,1,1);
+var cubo8= new THREE.BoxGeometry(1,1,1);
+var cubo9= new THREE.BoxGeometry(1,1,1);
+var cubo10= new THREE.BoxGeometry(1,1,1);
+var cubo11= new THREE.BoxGeometry(1,1,1);
+var cubo12= new THREE.BoxGeometry(1,1,1);
+var cubo12= new THREE.BoxGeometry(1,1,1);
+var cubo13= new THREE.BoxGeometry(1,1,1);
+var cubo14= new THREE.BoxGeometry(1,1,1);
+var cubo15= new THREE.BoxGeometry(1,1,1);
+var cubo16= new THREE.BoxGeometry(1,1,1);
+var cubo17= new THREE.BoxGeometry(1,1,1);
+var cubo18= new THREE.BoxGeometry(1,1,1);
+var cubo19= new THREE.BoxGeometry(1,1,1);
+var cubo20= new THREE.BoxGeometry(1,1,1);
+var cubo21= new THREE.BoxGeometry(1,1,1);
+var cubo22= new THREE.BoxGeometry(1,1,1);
+var cubo23= new THREE.BoxGeometry(1,1,1);
+var cubo24= new THREE.BoxGeometry(1,1,1);
+var cubo25= new THREE.BoxGeometry(1,1,1);
+var cubo26= new THREE.BoxGeometry(1,1,1);
+var cubo27= new THREE.BoxGeometry(1,1,1);
+var cubo28= new THREE.BoxGeometry(1,1,1);
+var cubo29= new THREE.BoxGeometry(1,1,1);
+var cubo30= new THREE.BoxGeometry(1,1,1);
+var cubo31= new THREE.BoxGeometry(1,1,1);
+var cubo32= new THREE.BoxGeometry(1,1,1);
+var cubo33= new THREE.BoxGeometry(1,1,1);
+var cubo34= new THREE.BoxGeometry(1,1,1);
+var cubo35= new THREE.BoxGeometry(1,1,1);
+var cubo36= new THREE.BoxGeometry(1,1,1);
+var cubo37= new THREE.BoxGeometry(1,1,1);
+var cubo38= new THREE.BoxGeometry(1,1,1);
+var cubo39= new THREE.BoxGeometry(1,1,1);
+var cubo40= new THREE.BoxGeometry(1,1,1);
+var cubo41= new THREE.BoxGeometry(1,1,1);
+var cubo42= new THREE.BoxGeometry(1,1,1);
+var cubo43= new THREE.BoxGeometry(1,1,1);
+var cubo44= new THREE.BoxGeometry(1,1,1);
+var cubo45= new THREE.BoxGeometry(1,1,1);
+var cubo46= new THREE.BoxGeometry(1,1,1);
+var cubo47= new THREE.BoxGeometry(1,1,1);
+var cubo48= new THREE.BoxGeometry(1,1,1);
+var cubo49= new THREE.BoxGeometry(1,1,1);
+var cubo50= new THREE.BoxGeometry(1,1,1);
+var cubo51= new THREE.BoxGeometry(1,1,1);
+var cubo52= new THREE.BoxGeometry(1,1,1);
+var cubo53= new THREE.BoxGeometry(1,1,1);
+var cubo54= new THREE.BoxGeometry(1,1,1);
+var cubo55= new THREE.BoxGeometry(1,1,1);
+var cubo56= new THREE.BoxGeometry(1,1,1);
+var cubo57= new THREE.BoxGeometry(1,1,1);
+var cubo58= new THREE.BoxGeometry(1,1,1);
+var cubo59= new THREE.BoxGeometry(1,1,1);
+var cubo60= new THREE.BoxGeometry(1,1,1);
+var cubo61= new THREE.BoxGeometry(1,1,1);
+var cubo62= new THREE.BoxGeometry(1,1,1);
+var cubo63= new THREE.BoxGeometry(1,1,1);
+var cubo64= new THREE.BoxGeometry(1,1,1);
 
-				var size = 500, step = 50;
 
-				var geometry = new THREE.BoxGeometry();
+cubo2.translate(1,0,0);
+cubo3.translate(2,0,0);
+cubo4.translate(3,0,0);
+cubo5.translate(4,0,0);
+cubo6.translate(5,0,0);
+cubo7.translate(6,0,0);
+cubo8.translate(7,0,0);
 
-				for ( var i = - size; i <= size; i += step ) {
+cubo9.translate(0,1,0);
+cubo10.translate(1,1,0);
+cubo11.translate(2,1,0);
+cubo12.translate(3,1,0);
+cubo13.translate(4,1,0);
+cubo14.translate(5,1,0);
+cubo15.translate(6,1,0);
+cubo16.translate(7,1,0);
 
-					geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
-					geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+cubo17.translate(0,2,0);
+cubo18.translate(1,2,0);
+cubo19.translate(2,2,0);
+cubo20.translate(3,2,0);
+cubo21.translate(4,2,0);
+cubo22.translate(5,2,0);
+cubo23.translate(6,2,0);
+cubo24.translate(7,2,0);
 
-					geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
-					geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+cubo25.translate(0,3,0);
+cubo26.translate(1,3,0);
+cubo27.translate(2,3,0);
+cubo28.translate(3,3,0);
+cubo29.translate(4,3,0);
+cubo30.translate(5,3,0);
+cubo31.translate(6,3,0);
+cubo32.translate(7,3,0);
 
-				}
+cubo33.translate(0,4,0);
+cubo34.translate(1,4,0);
+cubo35.translate(2,4,0);
+cubo36.translate(3,4,0);
+cubo37.translate(4,4,0);
+cubo38.translate(5,4,0);
+cubo39.translate(6,4,0);
+cubo40.translate(7,4,0);
 
-				var material = new THREE.LineBasicMaterial( { color: 0x888888, opacity: 0.2, transparent: true } );
+cubo41.translate(0,5,0);
+cubo42.translate(1,5,0);
+cubo43.translate(2,5,0);
+cubo44.translate(3,5,0);
+cubo45.translate(4,5,0);
+cubo46.translate(5,5,0);
+cubo47.translate(6,5,0);
+cubo48.translate(7,5,0);
 
-				var line = new THREE.LineSegments( geometry, material );
-				
-				line.rotateX(Math.PI/4);
-				
-				var escena = new THREE.Scene();
-				escena.add( line );
-				
-var camara = new THREE.PerspectiveCamera();
-camara.position.y = 5;
+cubo49.translate(0,6,0);
+cubo50.translate(1,6,0);
+cubo51.translate(2,6,0);
+cubo52.translate(3,6,0);
+cubo53.translate(4,6,0);
+cubo54.translate(5,6,0);
+cubo55.translate(6,6,0);
+cubo56.translate(7,6,0);
 
-renderizador = new THREE.WebGLRenderer();
-renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+cubo57.translate(0,7,0);
+cubo58.translate(1,7,0);
+cubo59.translate(2,7,0);
+cubo60.translate(3,7,0);
+cubo61.translate(4,7,0);
+cubo62.translate(5,7,0);
+cubo63.translate(6,7,0);
+cubo64.translate(7,7,0);
+
+var cubo1m= new THREE.Mesh(cubo1);
+var cubo2m= new THREE.Mesh(cubo2);
+var cubo3m= new THREE.Mesh(cubo3);
+var cubo4m= new THREE.Mesh(cubo4);
+var cubo5m= new THREE.Mesh(cubo5);
+var cubo6m= new THREE.Mesh(cubo6);
+var cubo7m= new THREE.Mesh(cubo7);
+var cubo8m= new THREE.Mesh(cubo8);
+var cubo9m= new THREE.Mesh(cubo9);
+var cubo10m= new THREE.Mesh(cubo10);
+var cubo11m= new THREE.Mesh(cubo11);
+var cubo12m= new THREE.Mesh(cubo12);
+var cubo13m= new THREE.Mesh(cubo13);
+var cubo14m= new THREE.Mesh(cubo14);
+var cubo15m= new THREE.Mesh(cubo15);
+var cubo16m= new THREE.Mesh(cubo16);
+var cubo17m= new THREE.Mesh(cubo17);
+var cubo18m= new THREE.Mesh(cubo18);
+var cubo19m= new THREE.Mesh(cubo19);
+var cubo20m= new THREE.Mesh(cubo20);
+var cubo21m= new THREE.Mesh(cubo21);
+var cubo22m= new THREE.Mesh(cubo22);
+var cubo23m= new THREE.Mesh(cubo23);
+var cubo24m= new THREE.Mesh(cubo24);
+var cubo25m= new THREE.Mesh(cubo25);
+var cubo26m= new THREE.Mesh(cubo26);
+var cubo27m= new THREE.Mesh(cubo27);
+var cubo28m= new THREE.Mesh(cubo28);
+var cubo29m= new THREE.Mesh(cubo29);
+var cubo30m= new THREE.Mesh(cubo30);
+var cubo31m= new THREE.Mesh(cubo31);
+var cubo32m= new THREE.Mesh(cubo32);
+var cubo33m= new THREE.Mesh(cubo33);
+var cubo34m= new THREE.Mesh(cubo34);
+var cubo35m= new THREE.Mesh(cubo35);
+var cubo36m= new THREE.Mesh(cubo36);
+var cubo37m= new THREE.Mesh(cubo37);
+var cubo38m= new THREE.Mesh(cubo38);
+var cubo39m= new THREE.Mesh(cubo39);
+var cubo40m= new THREE.Mesh(cubo40);
+var cubo41m= new THREE.Mesh(cubo41);
+var cubo42m= new THREE.Mesh(cubo42);
+var cubo43m= new THREE.Mesh(cubo43);
+var cubo44m= new THREE.Mesh(cubo44);
+var cubo45m= new THREE.Mesh(cubo45);
+var cubo46m= new THREE.Mesh(cubo46);
+var cubo47m= new THREE.Mesh(cubo47);
+var cubo48m= new THREE.Mesh(cubo48);
+var cubo49m= new THREE.Mesh(cubo49);
+var cubo50m= new THREE.Mesh(cubo50);
+var cubo51m= new THREE.Mesh(cubo51);
+var cubo52m= new THREE.Mesh(cubo52);
+var cubo53m= new THREE.Mesh(cubo53);
+var cubo54m= new THREE.Mesh(cubo54);
+var cubo55m= new THREE.Mesh(cubo55);
+var cubo56m= new THREE.Mesh(cubo56);
+var cubo57m= new THREE.Mesh(cubo57);
+var cubo58m= new THREE.Mesh(cubo58);
+var cubo59m= new THREE.Mesh(cubo59);
+var cubo60m= new THREE.Mesh(cubo60);
+var cubo61m= new THREE.Mesh(cubo61);
+var cubo62m= new THREE.Mesh(cubo62);
+var cubo63m= new THREE.Mesh(cubo63);
+var cubo64m= new THREE.Mesh(cubo64);
+
+var grises= new THREE.Geometry();
+grises.merge(cubo1m.geometry,cubo1m.matrix);
+grises.merge(cubo3m.geometry,cubo3m.matrix);
+grises.merge(cubo5m.geometry,cubo5m.matrix);
+grises.merge(cubo7m.geometry,cubo7m.matrix);
+grises.merge(cubo10m.geometry,cubo10m.matrix);
+grises.merge(cubo12m.geometry,cubo14m.matrix);
+grises.merge(cubo14m.geometry,cubo12m.matrix);
+grises.merge(cubo16m.geometry,cubo16m.matrix);
+grises.merge(cubo17m.geometry,cubo17m.matrix);
+grises.merge(cubo19m.geometry,cubo19m.matrix);
+grises.merge(cubo21m.geometry,cubo21m.matrix);
+grises.merge(cubo23m.geometry,cubo23m.matrix);
+grises.merge(cubo26m.geometry,cubo26m.matrix);
+grises.merge(cubo28m.geometry,cubo28m.matrix);
+grises.merge(cubo30m.geometry,cubo30m.matrix);
+grises.merge(cubo32m.geometry,cubo32m.matrix);
+grises.merge(cubo33m.geometry,cubo33m.matrix);
+grises.merge(cubo35m.geometry,cubo35m.matrix);
+grises.merge(cubo37m.geometry,cubo37m.matrix);
+grises.merge(cubo39m.geometry,cubo39m.matrix);//
+grises.merge(cubo42m.geometry,cubo42m.matrix);
+grises.merge(cubo44m.geometry,cubo44m.matrix);
+grises.merge(cubo46m.geometry,cubo46m.matrix);////
+grises.merge(cubo48m.geometry,cubo47m.matrix);
+grises.merge(cubo49m.geometry,cubo49m.matrix);
+grises.merge(cubo51m.geometry,cubo51m.matrix);
+grises.merge(cubo53m.geometry,cubo53m.matrix);
+grises.merge(cubo55m.geometry,cubo55m.matrix);//
+grises.merge(cubo58m.geometry,cubo57m.matrix);
+grises.merge(cubo60m.geometry,cubo60m.matrix);
+grises.merge(cubo62m.geometry,cubo62m.matrix);
+grises.merge(cubo64m.geometry,cubo64m.matrix);
+
+
+var blancos= new THREE.Geometry();
+blancos.merge(cubo2m.geometry,cubo2m.matrix);
+blancos.merge(cubo4m.geometry,cubo4m.matrix);
+blancos.merge(cubo6m.geometry,cubo6m.matrix);
+blancos.merge(cubo8m.geometry,cubo8m.matrix);
+blancos.merge(cubo9m.geometry,cubo9m.matrix);
+blancos.merge(cubo11m.geometry,cubo11m.matrix);
+blancos.merge(cubo13m.geometry,cubo13m.matrix);
+blancos.merge(cubo15m.geometry,cubo15m.matrix);
+blancos.merge(cubo18m.geometry,cubo18m.matrix);
+blancos.merge(cubo20m.geometry,cubo20m.matrix);
+blancos.merge(cubo22m.geometry,cubo22m.matrix);
+blancos.merge(cubo24m.geometry,cubo24m.matrix);
+blancos.merge(cubo25m.geometry,cubo25m.matrix);
+blancos.merge(cubo27m.geometry,cubo27m.matrix);
+blancos.merge(cubo29m.geometry,cubo29m.matrix);
+blancos.merge(cubo31m.geometry,cubo31m.matrix);
+blancos.merge(cubo34m.geometry,cubo34m.matrix);
+blancos.merge(cubo36m.geometry,cubo36m.matrix);
+blancos.merge(cubo38m.geometry,cubo38m.matrix);
+blancos.merge(cubo40m.geometry,cubo40m.matrix);
+blancos.merge(cubo41m.geometry,cubo41m.matrix);
+blancos.merge(cubo43m.geometry,cubo43m.matrix);
+blancos.merge(cubo45m.geometry,cubo45m.matrix);
+blancos.merge(cubo47m.geometry,cubo47m.matrix);
+blancos.merge(cubo50m.geometry,cubo50m.matrix);
+blancos.merge(cubo52m.geometry,cubo52m.matrix);
+blancos.merge(cubo54m.geometry,cubo54m.matrix);
+blancos.merge(cubo56m.geometry,cubo56m.matrix);
+blancos.merge(cubo57m.geometry,cubo57m.matrix);
+blancos.merge(cubo59m.geometry,cubo59m.matrix);
+blancos.merge(cubo61m.geometry,cubo61m.matrix);
+blancos.merge(cubo63m.geometry,cubo63m.matrix);
+
+var material1 = new THREE.MeshBasicMaterial({color: 0x868484});
+var material2 = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
+
+var grisesmalla= new THREE.Mesh(grises,material1);
+var blancosmalla= new THREE.Mesh(blancos,material2);
+
+///////////////////////////////////TORRE
+
+var p1Forma= new THREE.CylinderGeometry(.3,.3,.7);
+var p2Forma= new THREE.CylinderGeometry(.5,.5,.3);
+var p3Forma= new THREE.CylinderGeometry(.4,.5,.6);
+
+var p4Forma= new THREE.CylinderGeometry(0,.1,.2);
+var p5Forma= new THREE.CylinderGeometry(0,.1,.2);
+var p6Forma= new THREE.CylinderGeometry(0,.1,.2);
+var p7Forma= new THREE.CylinderGeometry(0,.1,.2);
+
+p1Forma.translate(.1,1,0);
+p2Forma.translate(.1,1,0);
+p3Forma.translate(.1,.3,0);
+
+p4Forma.translate(.1,.7,.3);
+p5Forma.translate(.1,.7,-.3);
+p6Forma.translate(.3,.7,.3);
+p7Forma.translate(.3,.7,-.3);
+
+var p1Malla= new THREE.Mesh(p1Forma);
+var p2Malla= new THREE.Mesh(p2Forma);
+var p3Malla= new THREE.Mesh(p3Forma);
+
+var p4Malla= new THREE.Mesh(p4Forma);
+var p5Malla= new THREE.Mesh(p5Forma);
+var p6Malla= new THREE.Mesh(p6Forma);
+var p7Malla= new THREE.Mesh(p7Forma);
+
+var torreForma= new THREE.Geometry();
+
+torreForma.merge(p1Malla.geometry,p1Malla.matrix);
+torreForma.merge(p2Malla.geometry,p2Malla.matrix);
+torreForma.merge(p3Malla.geometry,p3Malla.matrix);
+
+torreForma.merge(p4Malla.geometry,p4Malla.matrix);
+torreForma.merge(p5Malla.geometry,p5Malla.matrix);
+torreForma.merge(p6Malla.geometry,p6Malla.matrix);
+torreForma.merge(p7Malla.geometry,p7Malla.matrix);
+
+var material= new THREE.MeshNormalMaterial();
+var torreMalla= new THREE.Mesh(torreForma,material);
+torreMalla.rotateX(Math.PI/2);
+
+//////////////////////////////
+var escena= new THREE.Scene();
+escena.add(blancosmalla);
+escena.add(grisesmalla);
+escena.add(torreMalla);
+////////////////////////////////////CAMARA
+var campoVision= 60; //grados
+var relacionAspecto=window.innerWidth/window.innerHeight;
+var planoCercano=1;
+var planoLejano=100;
+var camara= new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano)
+
+camara.position.z=10;
+
+
+/////////////////////////////
+renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
-renderizador.render(escena, camara)
+renderizador.render(escena,camara)
+
+
