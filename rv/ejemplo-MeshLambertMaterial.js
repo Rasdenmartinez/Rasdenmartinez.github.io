02ef3f426cbe292c9581e5a9e715eb2ed0586db4
Rasdenmartinez <libre93@live.com.mx>
2016-09-20 15:33:30 -0500

    Update ejemplo-MeshLambertMaterial.js

--- rv/ejemplo-MeshLambertMaterial.js
+++ rv/ejemplo-MeshLambertMaterial.js
@@ -14,7 +14,7 @@ escena.add(LuzPuntual);
 var camara = new THREE.PerspectiveCamera();
 camara.position.z=5;
 
-//var lienzo = document.getElementById("LuzPuntual");
+var lienzo = document.getElementById("LuzPuntual");
 //var renderizador= new THREE.WebGLRenderer({canvas:lienzo, antialias:true});
 
 //renderizador.setSize(600,600);
