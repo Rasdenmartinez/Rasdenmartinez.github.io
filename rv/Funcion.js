1237dd3e021f9590fab05456c6114d4aebcc960c
Rasdenmartinez <libre93@live.com.mx>
2016-09-22 15:12:35 -0500

    Update Funcion.js

--- rv/Funcion.js
+++ rv/Funcion.js
@@ -1,6 +1,6 @@
 function init(p)
 {
-var malla (new THREE.BoxGeometry(p,p,p) new THREE.MeshNormalMaterial());
+var malla (new THREE.BoxGeometry(p,p,p), new THREE.MeshNormalMaterial());
 escena= new THREE.Scene();
 escena.add(malla);
 camara = new THREE.PerspectiveCamera();
