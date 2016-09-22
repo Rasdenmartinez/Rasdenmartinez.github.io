924a383660bbbdd29304d4afe8bd943f327d731f
Rasdenmartinez <libre93@live.com.mx>
2016-09-22 15:06:38 -0500

    Update Funcion.js

--- rv/Funcion.js
+++ rv/Funcion.js
@@ -1,14 +1,17 @@
-fuction init(p)
+function init(p)
 {
 var malla (new THREE.BoxGeometry(p,p,p) new THREE.MeshNormalMaterial());
 escena= new THREE.Scene();
-escena, add(malla);
+escena.add(malla);
 camara = new THREE.PerspectiveCamera();
 renderizador = new THREE.WebGLRender();
 renderizador.setSize(700,700);
-
+document.body.appendChild(renderizador.domElement);
 }
 var main = function(p)
 {
 renderizador.render(escena,camara);
 }
+var escena,camara, renderizador;
+init(1);
+main(2);
