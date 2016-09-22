06d93ed2546044a66fce1f84b30fc87cf2682a70
Rasdenmartinez <libre93@live.com.mx>
2016-09-22 14:56:13 -0500

    Create Funcion.js

---
+++ rv/Funcion.js
@@ -0,0 +1,14 @@
+fuction init(p)
+{
+var malla (new THREE.BoxGeometry(p,p,p) new THREE.MeshNormalMaterial());
+escena= new THREE.Scene();
+escena, add(malla);
+camara = new THREE.PerspectiveCamera();
+renderizador = new THREE.WebGLRender();
+renderizador.setSize(700,700);
+
+}
+var main = function(p)
+{
+renderizador.render(escena,camara);
+}
