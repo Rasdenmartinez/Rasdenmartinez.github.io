459d28eff95719ab485e7da0c95cc3e3fcd5f7a5
Rasdenmartinez <libre93@live.com.mx>
2016-10-27 15:54:14 -0500

    Update Robot.js

--- rv/Robot.js
+++ rv/Robot.js
@@ -67,8 +67,8 @@ Robot.prototype.sense=function(environment)
       var obstaculo = this.sensor.intersectObjects(environment.children,true);
       if(obstaculo.length>0 && (obstaculo[0].distance<= 0.5))
             
-}
-------------
+};
+
          Robot.prototype.plan=function(environment)
       {
             this actuator.comands = []:
