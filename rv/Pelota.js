2c7eac5c1c51631f8637aa40b1ff83c39939ed61
Rasdenmartinez <libre93@live.com.mx>
2016-11-10 15:49:58 -0600

    Update Pelota.js

--- rv/Pelota.js
+++ rv/Pelota.js
@@ -55,8 +55,7 @@ Pelota.prototype.sense=function(environment)
   this.sensor.set(this.position, new THREE.Vector3(-1,0,0));
   var obstaculo2= this.sensor.intersectObjects(environment.children,true);
  
- if ((obstaculo1.length>0 && (obstaculo1[0].distance <= this.radius)) || 
- (obstaculo2length>0 && (obstaculo2
+ if ((obstaculo1.length>0 && (obstaculo1[0].distance <= this.radius)) || (obstaculo2length>0 && (obstaculo2
                          [0].distance <= this.radius)))
  this.colision=1;
  else
