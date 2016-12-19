0a4416dae07d89ea80cb62526c962054d3cea2ce
Rasdenmartinez <libre93@live.com.mx>
2016-12-19 12:36:09 -0600

    Update tablero_movimiento_piezas_0.js

--- rv/tablero_movimiento_piezas_0.js
+++ rv/tablero_movimiento_piezas_0.js
@@ -295,21 +295,21 @@ Cursor.prototype.act = function(environment){
       var tecla = pieza.which;
         switch (tecla){
           case 37 : //Izquierda
-			
+		
 		if (bandera===1){
-		  if (seleccion.position.z<=-20){
-		    seleccion.translateZ(10);
+		  if (seleccion.position.z>=-70){
+		    seleccion.translateZ(-10);
 		  }
 		}
 		else{
 	  	   escena.remove(grupomorado);
 	           escena.remove(posicioninicial);
 	           escena.remove(seleccion);
-		   if (cursor.position.z<=-20){
-		     cursor.translateZ(10);
+		   if (cursor.position.z>=-70){
+	             cursor.translateZ(-10);
 		   }
 		}
-                break;
+			break;
 			
           case 38 :  //Arriba
 			
@@ -328,20 +328,21 @@ Cursor.prototype.act = function(environment){
 		}
                 break;
           case 39 :  //Derecha 
-		
+			
 		if (bandera===1){
-		  if (seleccion.position.z>=-70){
-		    seleccion.translateZ(-10);
+		  if (seleccion.position.z<=-20){
+		    seleccion.translateZ(10);
 		  }
 		}
 		else{
 	  	   escena.remove(grupomorado);
 	           escena.remove(posicioninicial);
 	           escena.remove(seleccion);
-		   if (cursor.position.z>=-70){
-	             cursor.translateZ(-10);
+		   if (cursor.position.z<=-20){
+		     cursor.translateZ(10);
 		   }
 		}
+                break;
           case 40 :  //Abajo
 			
 		if (bandera===1){
