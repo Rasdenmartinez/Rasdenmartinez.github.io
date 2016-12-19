ad7ec3826e5767845b91015bb3b2a4fda96bb92e
Rasdenmartinez <libre93@live.com.mx>
2016-12-19 02:47:27 -0600

    Update tablero_movimiento_piezas_0.js

--- rv/tablero_movimiento_piezas_0.js
+++ rv/tablero_movimiento_piezas_0.js
@@ -4001,9 +4001,8 @@ function setup() {
   
   //Luces
   escena.add(luzPuntual1, luzPuntual2, luzPuntual3);
-  escena.rotateX(Math.PI/2);
-escena.rotateZ(Math.PI/4);
-escena.rotateY(Math.PI/4);
+ // escena.rotateX(Math.PI/2);
+escena.rotateZ(Math.PI/2);
 }
 
 function loop() {
