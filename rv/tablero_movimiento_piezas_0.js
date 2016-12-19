736fa211c6cdea3b24396b2e942e4aceed5f99b9
Rasdenmartinez <libre93@live.com.mx>
2016-12-19 14:53:02 -0600

    Update tablero_movimiento_piezas_0.js

--- rv/tablero_movimiento_piezas_0.js
+++ rv/tablero_movimiento_piezas_0.js
@@ -1,4 +1,4 @@
-//Agente
+
 function Agent(x=0,y=0,z=0){
   THREE.Object3D.call(this);
   this.position.x=x;
@@ -8,20 +8,17 @@ function Agent(x=0,y=0,z=0){
 
 Agent.prototype = new THREE.Object3D();
 
-//Tres primitivas esenciales de un agente:
 Agent.prototype.sense = function(environment) {}; //Percibir
 Agent.prototype.plan = function(environment) {}; //Planificar
 Agent.prototype.act = function(environment) {}; //Actuar
 
-//Environment
-//Un Agente opera sobre un entorno
 function Environment(){
   THREE.Scene.call(this);
 }
 
 Environment.prototype = new THREE.Scene();
 
-//Preguntar a todos los agentes si sienten
+
 Environment.prototype.sense = function(){ 
   for(var i=0; i<this.children.length; i++){
     if(this.children[i].sense!==undefined)
