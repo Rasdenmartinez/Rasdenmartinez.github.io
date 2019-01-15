3e474cb12b559debd2adc2d9f493af7947120eec
Rasdenmartinez <libre93@live.com.mx>
2019-01-15 16:31:24 -0600

    Update app.js

--- Pruebas/sincronizacion_objetos_realtime/app.js
+++ Pruebas/sincronizacion_objetos_realtime/app.js
@@ -1,13 +1,13 @@
 (function(){
 
 // Initialize Firebase
-  const config = {
+  var config = {
     apiKey: "AIzaSyBoPPhIoKz6ZpDjdFNXu6SiP14ft-gZtFQ",
     authDomain: "iot2016-b397b.firebaseapp.com",
     databaseURL: "https://iot2016-b397b.firebaseio.com",
-    projectId: "iot2016-b397b",
+//    projectId: "iot2016-b397b",
     storageBucket: "iot2016-b397b.appspot.com",
-    messagingSenderId: "139828741250"
+  //  messagingSenderId: "139828741250"
   };
   firebase.initializeApp(config);
   
@@ -17,5 +17,8 @@
   const dbRefObject = firebase.database().ref().child('objeto');
   
   //sincronizar cambios objeto
-  dbRefObject.on('value', snap => console.log(snap.val()));
-  }());
+  dbRefObject.on('value', snap => {
+    preObject.innerText=JSON.stringify(snap.val(),null,3);
+  });
+  
+} ());
