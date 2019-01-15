fdd0b8d07e777fd9b82511c6d40cd0383261d144
Rasdenmartinez <libre93@live.com.mx>
2019-01-15 17:44:33 -0600

    Update app.js

--- Pruebas/usoChild/app.js
+++ Pruebas/usoChild/app.js
@@ -13,12 +13,21 @@
   
   //obtener elementos
   const preObject = document.getElementById('objeto');
+  const ulist = document.getElementById('lista');
   //crear referencias
   const dbRefObject = firebase.database().ref().child('objeto');
+  const dbReflist = dbRefObject.child('habilidades');
   
   //sincronizar cambios objeto
   dbRefObject.on('value', snap => {
     preObject.innerText=JSON.stringify(snap.val(),null,3);
   });
   
-} ());
+  //sincronizar cambios lista
+  dbRefList.on('child_added', snap=>{
+    const li = document.createElement('li');
+    li.innerText = snap.val();
+    ulist.appendChild(li);
+  }
+  
+());
